fc432c8ef7a15b4c9429d36eb5121a1c4a956949
mtempleton94 <mitchelltempleton94@gmail.com>
2015-04-08 17:08:30 +0930

    Start of recycler list view

--- app/src/main/java/com/example/mitchell/navigationdrawer/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/mitchell/navigationdrawer/NavigationDrawerFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,8 @@ import android.support.v7.widget.Toolbar;
  * A simple {@link Fragment} subclass.
  */
 public class NavigationDrawerFragment extends Fragment {
+
+    private RecyclerView recyclerView;
 
     public static final String PREF_FILE_NAME="testPref";
     public static final String KEY_USER_LEARNED_DRAWER="user_learned_drawer";
@@ -49,7 +52,10 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+
+        View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        recyclerView = (RecyclerView)layout.findViewById(R.id.drawerList);
+        return layout;
     }
 
 
